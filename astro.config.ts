6614baf9aabdf2b88041cb9749375bce5652c28f
YuLiang Zhu <zhuyuliangm@outlook.com>
2024-12-31 07:33:03 +0000

    add vite plugin

--- astro.config.ts
+++ astro.config.ts
@@ -1,3 +1,5 @@
+import ChildProcess from 'node:child_process'
+import { promisify } from 'node:util'
 import db from '@astrojs/db'
 import mdx from '@astrojs/mdx'
 import netlify from '@astrojs/netlify'
@@ -20,6 +22,14 @@ import rehypeExternalLinks from 'rehype-external-links'
 import rehypeSlug from 'rehype-slug'
 
 import { schema } from './env.schema'
+
+const execAsync = promisify(ChildProcess.exec)
+
+async function generateGitData() {
+  return (await execAsync('git rev-parse HEAD')).stdout
+}
+
+const res = JSON.stringify(await generateGitData())
 
 /**
  * https://astro.build/config
@@ -79,7 +89,7 @@ export default defineConfig({
         transformerMetaWordHighlight(),
         {
           pre(node) {
-            node.properties.__lang__ = this.options.lang
+            node.properties.__meta__ = this.options.meta?.__raw
             node.properties.__rawString__ = this.source
           },
         },
@@ -128,6 +138,17 @@ export default defineConfig({
     optimizeDeps: {
       include: ['lucide-react'],
     },
+    plugins: [{
+      name: 'vite-plugin-git-revision-info',
+      config() {
+        return {
+          // 全局变量，可以在整个应用中使用
+          define: {
+            PUBLIC_GIT_REVISION_INFO: res,
+          },
+        }
+      },
+    }],
   },
 
   env: {
