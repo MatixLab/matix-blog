cc4187bf5a5d0a7de1d7a78d71b78a3cf997ede6
YuLiang Zhu <zhuyuliangm@outlook.com>
2025-02-17 10:16:04 +0000

    update

--- src/components/markdown/mdx-components.tsx
+++ src/components/markdown/mdx-components.tsx
@@ -62,7 +62,7 @@ export const MdxComponents = {
   ),
   a: ({ className, ...props }: Props) => (
     <a
-      className={cn('no-underline underline-offset-4 text-ds-gray-1000', className)}
+      className={cn('text-gray-600 underline underline-offset-4 decoration-ds-gray-500 hover:decoration-ds-gray-900 duration-500 ease-in-out decoration-[1.5px] px-1', className)}
       {...props}
     >
     </a>
