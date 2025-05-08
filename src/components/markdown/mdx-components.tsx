0ae92c1b5399dbf1f969a86c78ceb72efd611023
huakucha <zhuyuliangm@outlook.com>
2025-05-08 21:32:51 +0800

    update

--- src/components/markdown/mdx-components.tsx
+++ src/components/markdown/mdx-components.tsx
@@ -8,63 +8,42 @@ interface Props {
 export const MdxComponents = {
   h1: ({ className, ...props }: Props) => (
     <h1
-      className={cn(
-        'mt-2 scroll-m-20 text-4xl  font-semibold ',
-        className,
-      )}
+      className={cn('mt-2 scroll-m-20 text-4xl font-semibold', className)}
       {...props}
     />
   ),
   h2: ({ className, ...props }: Props) => (
     <h2
-      className={cn(
-        'mt-10 scroll-m-20 pb-1 text-2xl font-semibold',
-        className,
-      )}
+      className={cn('mt-10 scroll-m-20 pb-1 text-2xl font-semibold', className)}
       {...props}
     />
   ),
   h3: ({ className, ...props }: Props) => (
     <h3
-      className={cn(
-        'mt-8 scroll-m-20 text-2xl font-semibold ',
-        className,
-      )}
+      className={cn('mt-8 scroll-m-20 text-2xl font-semibold', className)}
       {...props}
     />
   ),
   h4: ({ className, ...props }: Props) => (
     <h4
-      className={cn(
-        'mt-8 scroll-m-20 text-xl font-semibold ',
-        className,
-      )}
+      className={cn('mt-8 scroll-m-20 text-xl font-semibold', className)}
       {...props}
     />
   ),
   h5: ({ className, ...props }: Props) => (
     <h5
-      className={cn(
-        'mt-8 scroll-m-20 text-lg font-semibold ',
-        className,
-      )}
+      className={cn('mt-8 scroll-m-20 text-lg font-semibold', className)}
       {...props}
     />
   ),
   h6: ({ className, ...props }: Props) => (
     <h6
-      className={cn(
-        'mt-8 scroll-m-20 text-base font-semibold ',
-        className,
-      )}
+      className={cn('mt-8 scroll-m-20 text-base font-semibold', className)}
       {...props}
     />
   ),
   p: ({ className, ...props }: Props) => (
-    <p
-      className={cn('leading-7 not-first:mt-6', className)}
-      {...props}
-    />
+    <p className={cn('leading-7 not-first:mt-6', className)} {...props} />
   ),
   ul: ({ className, ...props }: Props) => (
     <ul className={cn('my-6 ml-1 list-disc', className)} {...props} />
@@ -79,7 +58,7 @@ export const MdxComponents = {
     <blockquote
       className={cn(
         'mt-6 border-l-2 pl-6 italic *:text-muted-foreground',
-        className,
+        className
       )}
       {...props}
     />
@@ -100,7 +79,7 @@ export const MdxComponents = {
     <th
       className={cn(
         'border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right',
-        className,
+        className
       )}
       {...props}
     />
@@ -109,9 +88,9 @@ export const MdxComponents = {
     <td
       className={cn(
         'border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right',
-        className,
+        className
       )}
       {...props}
     />
-  ),
+  )
 }
